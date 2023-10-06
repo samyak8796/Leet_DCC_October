b38e70a105cc819b994c9fd39d023919bc791151
Samyak Jain <115895047+samyak8796@users.noreply.github.com>
2023-10-06 09:10:12 +0530

    Posting a solution after a while

---
+++ 2_6oct_343.cpp
@@ -0,0 +1,13 @@
+class Solution{
+public:
+    int integerBreak(int n) {
+        if (n==2 || n== 3) 
+            return (n-1);
+        int ans = 1;
+        while(n>4){
+            n -= 3;
+            ans *= 3; 
+        }
+        return (n*ans);
+    }
+};
